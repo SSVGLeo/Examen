cc6958d3fbe5c33a3ec5817f040bf76187bf790b
SSVGLeo <troupleo@gmail.com>
2025-12-03 10:41:59 +0100

    Ajout de mongoDB npm install mongodb
    Rajout du code pour se connecter à la base de donnée
    Verification dans la console nodejs du message de confirmation

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+// On va importer MongoDB avec require pour pouvoir se connecter à la base de données de l'éxamen
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// Dans la variable uri on renseigne le lien de connexion à la base de données avec le username et le userpassword (pacomesuperbg)
+const uri = "mongodb+srv://troupleo_db_user2:pacomesuperbg@dbexamen0312.9ab96p0.mongodb.net/?appName=DBExamen0312";
+
+// Le code fournit par MongoAtlas pour se connecter à la BDD, en gros on fait un "client" qui se connecte avec le lien URI
+// Et on va faire une fonction asynchrone qui va se connecter à la BDD et qui envoie un message dans la console si ça a bien marché
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    // Changement du message pour quelque chose de plus parlant
+    console.log("Connexion réussit, Pacome est bien Super BG !");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 // On va importer Express avec require pour pouvoir l'utiliser
 const express = require('express');
 // On définit notre application avec express()
